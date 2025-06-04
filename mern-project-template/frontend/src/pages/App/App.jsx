a1060868d0f1148a32c9228f3e9264198f70c951
soltan <soltan@Mac.attlocal.net>
2025-06-04 14:09:12 -0500

    mern-project-template

---
+++ mern-project-template/frontend/src/pages/App/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router";
+import { getUser } from "../../services/authService";
+import HomePage from "../HomePage/HomePage";
+import PostListPage from "../PostListPage/PostListPage";
+import NewPostPage from "../NewPostPage/NewPostPage";
+import SignUpPage from "../SignUpPage/SignUpPage";
+import NavBar from "../../components/NavBar/NavBar";
+import "./App.css";
+
+export default function App() {
+  const [user, setUser] = useState(getUser());
+
+  return (
+    <main className="App">
+      <NavBar user={user} />
+      <section id="main-section">
+        {user ? (
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/posts" element={<PostListPage />} />
+            <Route path="/posts/new" element={<NewPostPage />} />
+          </Routes>
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/signup" element={<SignUpPage setUser={setUser} />} />
+          </Routes>
+        )}
+      </section>
+    </main>
+  );
+}
